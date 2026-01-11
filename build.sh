4cb6516052038a139e0bef3066c4fe97e9f768e1
Adhavan J.V.R <adhavanravikrishna@gmail.com>
2026-01-11 20:20:27 +0530

    Finalize dev branch fixes

--- build.sh
+++ build.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-docker build -t react-app:$1 .
+set -e
+
+IMAGE_NAME=$1
+TAG=$2
+
+docker pull ${IMAGE_NAME}:${TAG}
+
+docker stop react-app || true
+docker rm react-app || true
+
+docker run -d \
+  --name react-app \
+  -p 80:80 \
+  --restart always \
+  ${IMAGE_NAME}:${TAG}
+
