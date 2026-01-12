042e0932554ab39d5f32ed69f867f0d23171b63e
Adhavan J.V.R <adhavanravikrishna@gmail.com>
2026-01-12 13:49:33 +0530

    seprarte build adn deploy logic crtly

--- build.sh
+++ build.sh
@@ -4,14 +4,11 @@ set -e
 IMAGE_NAME=$1
 TAG=$2
 
-docker pull ${IMAGE_NAME}:${TAG}
+if [ -z "$IMAGE_NAME" ] || [ -z "$TAG" ]; then
+  echo "Usage: ./build.sh <image_name> <tag>"
+  exit 1
+fi
 
-docker stop react-app || true
-docker rm react-app || true
-
-docker run -d \
-  --name react-app \
-  -p 80:80 \
-  --restart always \
-  ${IMAGE_NAME}:${TAG}
+docker build -t ${IMAGE_NAME}:${TAG} .
+docker push ${IMAGE_NAME}:${TAG}
 
